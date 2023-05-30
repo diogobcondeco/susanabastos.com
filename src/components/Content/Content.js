71d9fc3b359bb6afe59e041b02e750545e28994e
Diogo Condêço <diogobcondeco@gmail.com>
2023-05-30 13:38:09 +0100

    making the gap better between on socials

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -12,7 +12,7 @@ const Content = () => {
     <div className='relative z-10 text-center text-white'>
       <h1 className='text-4xl font-bold'>Susana Bastos</h1>
       <p className='mt-1'>Hipnoterapia • PNL</p>
-      <div className='flex items-center justify-center gap-8 my-4 text-4xl'>
+      <div className='flex items-center justify-center gap-4 my-4 text-4xl'>
         <SocialLink link="https://www.facebook.com/susanabastoshipnose" icon={faFacebook} />
         <SocialLink link="https://wa.me/352691108813" icon={faWhatsapp} />
         <SocialLink link="https://t.me/susanabastos" icon={faTelegram} />
