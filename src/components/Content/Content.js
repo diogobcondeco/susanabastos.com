1944f4ce25716359f0293b3eaf33a52ce6f46a07
Diogo Condêço <diogobcondeco@gmail.com>
2023-05-30 13:31:35 +0100

    changing text to pt and removing scroll

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -3,18 +3,20 @@ import SocialLink from './SocialLink/SocialLink';
 import {
   faFacebook,
   faWhatsapp,
-  faTelegram
+  faTelegram,
+  faInstagram
 } from '@fortawesome/free-brands-svg-icons'
 
 const Content = () => {
   return (
     <div className='relative z-10 text-center text-white'>
       <h1 className='text-4xl font-bold'>Susana Bastos</h1>
-      <p className='mt-1'>Meditation • Reiki • Hypnotherapy</p>
+      <p className='mt-1'>Hipnoterapia • PNL</p>
       <div className='flex items-center justify-center gap-8 my-4 text-4xl'>
         <SocialLink link="https://www.facebook.com/susanabastoshipnose" icon={faFacebook} />
         <SocialLink link="https://wa.me/352691108813" icon={faWhatsapp} />
         <SocialLink link="https://t.me/susanabastos" icon={faTelegram} />
+        <SocialLink link="https://www.instagram.com/susanabastoshipnose/" icon={faInstagram} />
       </div>
     </div>
   )
